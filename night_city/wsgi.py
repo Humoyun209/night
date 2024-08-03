abe223a20522fba3ac79febbc09dbd3450a72709
humoyun209 <h.ahmedov209@gmail.com>
2024-08-03 20:36:22 +0500

    Added

--- night_city/wsgi.py
+++ night_city/wsgi.py
@@ -8,13 +8,30 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from environs import Env
 from django.core.wsgi import get_wsgi_application
+from django.urls import path
 from whitenoise import WhiteNoise
+
+env = Env()
+env.read_env()
+MEDIA_PATH = env.str("NOICE_MEDIA_FILES")
+STATIC_PATH = env.str("NOICE_STATIC_FILES")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "night_city.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(
-    application, root="/home/humoyun209/Рабочий стол/python/night_city/static"
-)
+
+urlpatterns = [
+    path(
+        "static/",
+        WhiteNoise(application, root=STATIC_PATH),
+    ),
+    path(
+        "media/",
+        WhiteNoise(application, root=MEDIA_PATH),
+    ),
+]
+
+application = WhiteNoise(application, root=STATIC_PATH)
+application.add_files(MEDIA_PATH, prefix="media/")
