b2c2951f4c9a1016b7be2aff86415deb39ffbfe7
humoyun209 <h.ahmedov209@gmail.com>
2024-08-24 22:49:30 +0500

    Added prtfolio

--- city_app/urls.py
+++ city_app/urls.py
@@ -6,4 +6,7 @@ from city_app import views
 urlpatterns = [
     path("catalog/", views.catalog_view, name="home"),
     path("", views.choose_city_view, name="city"),
+    path("portfolio/", views.get_portfolio, name="portfolio"),
+    path("portfolio/vmware-vsphere/", views.get_portfolio1, name="portfolio1"),
+    path("portfolio/hunter-3d/", views.get_portfolio2, name="portfolio2"),
 ]
--- city_app/views.py
+++ city_app/views.py
@@ -8,3 +8,16 @@ def catalog_view(request: HttpRequest):
 
 def choose_city_view(request: HttpRequest):
     return render(request, "city_app/index.html")
+
+
+def get_portfolio(request: HttpRequest):
+    return render(request, "portfolio/portfolio.html")
+
+
+def get_portfolio1(request: HttpRequest):
+    return render(request, "portfolio/portfolio1.html")
+
+
+def get_portfolio2(request: HttpRequest):
+    return render(request, "portfolio/portfolio2.html")
+
